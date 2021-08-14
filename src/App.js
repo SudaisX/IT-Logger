d6cbd49a80d47e22653a433fa1fa3ee4646571a6
Sudais <44402465+sudaisx@users.noreply.github.com>
2021-08-14 22:48:02 +0500

    frontend

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import React, { useEffect } from 'react';
+
+import 'materialize-css/dist/css/materialize.min.css';
+import M from 'materialize-css/dist/js/materialize.min.js';
+
+import SearchBar from './components/layout/SearchBar';
+import Logs from './components/logs/Logs';
+import AddBtn from './components/layout/AddBtn';
+import AddLogModal from './components/logs/AddLogModal';
+import EditLogModal from './components/logs/EditLogModal';
+import AddTechModal from './components/techs/AddTechModal';
+import TechListModal from './components/techs/TechListModal';
+
+const App = () => {
+    useEffect(() => {
+        M.AutoInit(); // Init Materialize JS
+    });
+
+    return (
+        <>
+            <SearchBar />
+            <div className='container'>
+                <AddBtn />
+                <AddLogModal />
+                <AddTechModal />
+                <EditLogModal />
+                <TechListModal />
+                <Logs />
+            </div>
+        </>
+    );
+};
+
+export default App;
